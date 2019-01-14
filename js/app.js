424af0c38ce5cf97ba532538d4d79379d12ea0ce
13660539818 <13660539818@139.com>
2019-01-14 22:37:46 +0800

    add/del/toggle/

--- js/app.js
+++ js/app.js
@@ -1,6 +1,52 @@
-(function (window) {
+(function (exports) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	exports.vm = new Vue({
+		el: ".todoapp",
+		data: {
+			inputValue: "",
+			lists: [
+				{
+					id: 1,
+					todo: "study",
+					isFinshed: false
+				},
+				{
+					id: 2,
+					todo: "coding",
+					isFinshed: true
+				},
+			],
+			flag: true
+		},
+		methods: {
+			add() {
+				if (!this.inputValue) {
+					return;
+				}
+				this.lists.push({
+					id: this.lists.length + 1,
+					todo: this.inputValue,
+					isFinshed: false
+				});
+				this.inputValue = "";
+			},
+			del(id) {
+				var index = this.lists.findIndex(item => {
+					if (item.id == id) {
+						return true;
+					}
+				});
+				this.lists.splice(index, 1);
+			},
+			toggleAll() {
+				this.lists.forEach(element => {
+					element.isFinshed = this.flag;
+				});
+				this.flag = !this.flag;
+			}
+			
+		}
+	});
 
 })(window);
